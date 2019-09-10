aa1a6cec5af86d96efa540e1955175df3dc93fff
tfried2001 <tfried_2001@yahoo.com>
2019-09-10 10:21:35 -0400

    M08-L07 with invitation view play

--- player/models.py
+++ player/models.py
@@ -1,10 +1,32 @@
 from django.db import models
 
+from django.db.models import Q as orQuery
 from django.contrib.auth.models import User
 # Create your models here.
+
+class InvitationQuerySet(models.QuerySet):
+    def my_invitations(self, user):
+        return self.filter(
+            orQuery( from_user = user )
+        )
+
+    def sent_invitations(self, user):
+        return self.filter(
+            orQuery( to_user = user )
+        )
+
 
 class Invitation(models.Model):
     from_user = models.ForeignKey(User, related_name="invitations_sent", on_delete=models.CASCADE)
     to_user = models.ForeignKey(User, related_name="invitations_received", on_delete=models.CASCADE)
     message = models.CharField(max_length=300)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = InvitationQuerySet.as_manager()
+
+    def __str__(self):
+        return "From: {0}, To: {1}, Message: {2}".format(
+            self.from_user,
+            self.to_user,
+            self.message
+        )
--- player/views.py
+++ player/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.decorators import login_required
+from .models import Invitation
 from .forms import InvitationForm
 from gameplay.models import Game
 
@@ -11,13 +12,24 @@ from gameplay.models import Game
 def home(request):
     my_games = Game.objects.games_for_user(request.user)
     active_games = my_games.active()
+    my_invitations = Invitation.objects.my_invitations(request.user)
+    sent_invitations = Invitation.objects.sent_invitations(request.user)
 
     return render(request, 'player/home.html',
-                  {'activeGames': active_games, 'myGames': my_games})
+                  {'activeGames': active_games,
+                  'myGames': my_games,
+                  'myInvitations': my_invitations,
+                  'sentInvitations': sent_invitations})
 
 
 @login_required
 def new_invitation(request):
-    form = InvitationForm()
+    if request.method == "POST":
+        invitation = Invitation(from_user=request.user)
+        form = InvitationForm(instance=invitation, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('player_home')
+    else:
+        form = InvitationForm()
     return render(request, 'player/new_invitation_form.html', {'form': form})
-    
